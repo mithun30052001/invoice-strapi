766746baa3d6fb0c5632dd341415e80f9ad82aef
mithun30052001 <lmithun01@gmail.com>
2025-05-25 14:22:17 +0530

    host changed to ipv4

--- config/database.js
+++ config/database.js
@@ -2,13 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'db.mtkfbeahzpzftxfoqmef.supabase.co'),
+      host: env('DATABASE_HOST','172.64.149.246'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'invoice_currency'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true)
+        ? {
+            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+          }
+        : false,
     },
-    useNullAsDefault: true,
   },
 });
