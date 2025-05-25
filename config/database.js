7b6c9a6f560d5e663c5908b465b8ee4cf7b0c8de
mithun30052001 <lmithun01@gmail.com>
2025-05-25 14:58:39 +0530

    changes made to host from session pool

--- config/database.js
+++ config/database.js
@@ -2,20 +2,19 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST','172.64.149.246'),
+      host: env('DATABASE_HOST', 'aws-0-ap-southeast-1.pooler.supabase.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'invoice_currency'),
-      ssl: env.bool('DATABASE_SSL', true)
-        ? {
-            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-          }
-        : false,
+      user: env('DATABASE_USERNAME', 'postgres.mtkfbeahzpzftxfoqmef'),
+      password: env('DATABASE_PASSWORD','invoice_currency'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 0,
       max: 5,
     },
+    debug: false,
   },
 });
